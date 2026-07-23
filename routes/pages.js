0cbc6e351b3068a6e3201951511dd6e9a21e9168
zain jafar <zain.jaffar@algorepublic.com>
2026-07-23 14:08:14 +0500

    static pages style issue fixed

--- routes/pages.js
+++ routes/pages.js
@@ -1,17 +1,33 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
 const router = express.Router();
 const pagesDir = path.join(__dirname, "../public/pages");
+const stylesheetPath = path.join(pagesDir, "styles.css");
 
-router.get("/terms-and-conditions", (req, res) => {
-  res.sendFile(path.join(pagesDir, "terms-and-conditions.html"));
+const readStylesheet = () => {
+  return fs.readFileSync(stylesheetPath, "utf8");
+};
+
+const serveLegalPage = (fileName) => (req, res) => {
+  const htmlPath = path.join(pagesDir, fileName);
+  let html = fs.readFileSync(htmlPath, "utf8");
+  const styles = readStylesheet();
+
+  html = html.replace(
+    '<link rel="stylesheet" href="/pages/styles.css" />',
+    `<style>${styles}</style>`,
+  );
+
+  res.type("html").send(html);
+};
+
+router.get("/terms-and-conditions", serveLegalPage("terms-and-conditions.html"));
+router.get("/privacy-policy", serveLegalPage("privacy-policy.html"));
+
+router.get("/styles.css", (req, res) => {
+  res.type("css").send(readStylesheet());
 });
-
-router.get("/privacy-policy", (req, res) => {
-  res.sendFile(path.join(pagesDir, "privacy-policy.html"));
-});
-
-router.use(express.static(pagesDir));
 
 module.exports = router;
